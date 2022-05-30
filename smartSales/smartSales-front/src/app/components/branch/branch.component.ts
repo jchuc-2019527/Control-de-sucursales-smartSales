9066a1fa181e37b15d92881f12674dda8c987ea1
rhernandez-2017470 <rhernandez-2017470@kinal.edu.gt>
2022-05-30 16:26:14 -0600

    Se agregaron funciones de Branch

--- smartSales/smartSales-front/src/app/components/branch/branch.component.ts
+++ smartSales/smartSales-front/src/app/components/branch/branch.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { branchModel } from 'src/app/models/branch.model';
+import { companyModel } from 'src/app/models/company.model';
+import { BranchRestService } from 'src/app/services/branchRest/branch-rest.service';
+import { CompanyRestService } from 'src/app/services/companyRest/company-rest.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-branch',
@@ -6,10 +11,92 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./branch.component.css']
 })
 export class BranchComponent implements OnInit {
+  branchs: any =[] ;
+  branch: branchModel;
+  branchUpdate: any;
+  name: string='';
+  company: any;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private branchRest: BranchRestService,
+    private companyRest: CompanyRestService
+  ) { 
+    this.branch = new branchModel('','','','');
+    this.company = new companyModel('','','','','','');
   }
 
+  ngOnInit(): void {
+    this.getBranchs(),
+    this.name = this.companyRest.getIdentity().name
+  }
+
+  getBranchs(){
+    this.branchRest.getBranchs().subscribe({
+      next:(res:any)=>{
+        this.branchs = res.branchs;
+        console.log(this.branchs);
+      },
+      error: (err) => console.log(err.error.message || err.error)
+    })
+  };
+
+  addBranch(addBranchForm:any){
+    this.branchRest.addBranch(this.branch).subscribe({
+      next:(res:any)=>{
+        Swal.fire({
+          title: res.message + '  ' + res.branch.name,
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 2000,
+          position:'center'
+        })
+        this.getBranchs()
+        addBranchForm.reset()
+      },
+      error: (err) => alert(err.error.message || err.error)
+    })
+  };
+
+  getBranch(id: string){
+    this.branchRest.getBranch(id).subscribe({
+      next:(res:any)=>{this.branchUpdate = res.branch},
+      error:(err)=>{alert(err.error.message)}
+    })
+  };
+
+  updateBranch(){
+    this.branchRest.updateBranch(this.branchUpdate._id, this.branchUpdate).subscribe({
+      next:(res:any)=>{
+        Swal.fire({
+          title: res.message + '  ' + res.branchUpdate.name,
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 2000,
+          position:'center'
+        })
+        this.getBranchs()},
+      error:(err)=> alert(err.error.message || err.error)
+    })
+  };
+
+  deleteBranch(id: string){
+    this.branchRest.deleteBranch(id).subscribe({
+      next:(res:any)=>{
+        Swal.fire({
+          title: res.message + '  ' + res.deleteBranch.name,
+          icon: 'success',
+          showConfirmButton: false,
+          timer: 2000,
+          position:'center'
+        })
+        this.getBranchs();
+      },
+      error:(err)=>Swal.fire({
+        title: err.error.message,
+        icon: 'error',
+        timer: 4000,
+        position:'center'
+      })
+    })
+  };
 }
--- smartSales/smartSales-front/src/app/services/branchRest/branch-rest.service.ts
+++ smartSales/smartSales-front/src/app/services/branchRest/branch-rest.service.ts
@@ -1,9 +1,39 @@
 import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AdminRestService } from '../adminRest/admin-rest.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class BranchRestService {
+  httOptions = new HttpHeaders({
+    'Content-Type': 'application/json',
+    'Authorization': this.adminRest.getToken()
+  })
+  constructor(
+    private http: HttpClient,
+    private adminRest: AdminRestService
+  ) { }
 
-  constructor() { }
+  getBranchs(){
+    return this.http.get(environment.baseUrl + 'branch/getBranchs',{headers:this.httOptions});
+  };
+
+  addBranch(params: {}){
+    return this.http.post(environment.baseUrl + 'branch/addBranch' ,params, {headers:this.httOptions});
+  };
+
+  getBranch(id: string){
+    return this.http.get(environment.baseUrl + 'branch/getBranch/' + id, {headers:this.httOptions});
+  };
+
+  updateBranch(id:string, params:{}){
+    return this.http.put(environment.baseUrl + 'branch/updateBranch/' + id, params,  {headers: this.httOptions});
+   };
+
+   deleteBranch(id: string){
+    return this.http.delete(environment.baseUrl + 'branch/deleteBranch/' + id, {headers: this.httOptions});
+   };
 }
+
